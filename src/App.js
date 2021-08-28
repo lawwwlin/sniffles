313c7bb6e0375fde979f10f74b5c4f3834735d27
cthu97 <85130439+cthu97@users.noreply.github.com>
2021-08-28 09:27:48 -0700

    backend-frontend connection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
-    </div>
-  );
-}
 
+    const [socket, setSocket] = useState(null);
+  
+     useEffect(() => {
+      const newSocket = io(`http://${window.location.hostname}:3001`);
+      setSocket(newSocket);
+      return () => newSocket.close();
+    }, [setSocket]);
+ 
+    return (
+      <div className="App">
+        <header className="app-header">
+          Connected?
+        </header>
+        { socket ? (
+          <div className="chat-container">
+            <h3>Connected</h3>
+          </div>
+        ) : (
+          <div>Not Connected</div>
+        )}
+      </div>
+    );
+  }
 export default App;
