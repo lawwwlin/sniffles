feed5f208bf9026ca7ac0de8e8203e947d01cfc1
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-08-29 10:00:35 -0700

    add reset database

--- backend-server/routes/profile.js
+++ backend-server/routes/profile.js
@@ -3,16 +3,17 @@ var router = express.Router();
 const db = require("../db/index");
 
 /* GET users listing. */
-router.get("/", (req, res) => {
-  //console.log("we are hitting the appointments route")
-  db.query("Select * from profile")
-    .then((data) => {
-      const profiles = data.rows;
-      res.json(profiles);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-});
-
-module.exports = router;
+module.exports = db => {
+  router.get("/", (req, res) => {
+    //console.log("we are hitting the appointments route")
+    db.query("Select * from profile")
+      .then((data) => {
+        const profiles = data.rows;
+        res.json(profiles);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
+};
