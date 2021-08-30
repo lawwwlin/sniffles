e81a573e41fe08a67626a73f2d852dc32e8fd893
Antony <ac.chan@live.com>
2021-08-30 02:48:52 +0000

    create route to profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import TopNav from "./TopNav";
+import Profile from "./Profile";
+import io from "socket.io-client";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const [socket, setSocket] = useState(null);
 
-    const [socket, setSocket] = useState(null);
-  
-     useEffect(() => {
-      const newSocket = io(`http://${window.location.hostname}:3001`);
-      setSocket(newSocket);
-      return () => newSocket.close();
-    }, [setSocket]);
- 
-    return (
-      <div className="App">
-        <header className="app-header">
-          Connected?
-        </header>
-        { socket ? (
-          <div className="chat-container">
-            <h3>Connected</h3>
-          </div>
-        ) : (
-          <div>Not Connected</div>
-        )}
-      </div>
-    );
-  }
+  useEffect(() => {
+    const newSocket = io(`http://${window.location.hostname}:3001`);
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [setSocket]);
+
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/profile">
+            <TopNav />
+            <Profile />
+          </Route>
+
+          <Route path="/">
+            <TopNav />
+            <header className="app-header">Connected?</header>
+            {socket ? (
+              <div className="chat-container">
+                <h3>Connected</h3>
+              </div>
+            ) : (
+              <div>Not Connected</div>
+            )}
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
 export default App;
---
+++ frontend/src/TopNav.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./TopNav.css";
+import PersonIcon from "@material-ui/icons/Person";
+import TextsmsIcon from "@material-ui/icons/Textsms";
+import IconButton from "@material-ui/core/IconButton";
+import { Link } from "react-router-dom";
+
+
+function TopNav() {
+  return (
+    <div className="topnav">
+      
+      <Link to="/profile">
+        <IconButton>
+          <PersonIcon className="topnav_icon" fontSize="large" />
+        </IconButton>
+        </Link>
+      
+
+      <Link to="/">
+      <IconButton>
+        <h2>TOP NAV: LOGO AREA</h2>
+      </IconButton>
+      </Link>
+      
+
+      <IconButton>
+        <TextsmsIcon className="topnav_icon" fontSize="large" />
+      </IconButton>
+    </div>
+  );
+}
+
+export default TopNav;
