469f85a535b28cbdbe07a056d5f66262205fbdbd
cthu97 <85130439+cthu97@users.noreply.github.com>
2021-08-31 11:37:54 -0700

    changed port/ ????

--- src/components/MessageScreen.js
+++ src/components/MessageScreen.js
@@ -3,34 +3,59 @@ import "./MessageScreen.css";
 
 const candidate = {
   name: "dog1",
-  matchtime: "06/09/21"
-}
-
+  matchtime: "06/09/21",
+};
 
 function MessageScreen(props) {
+  let socket;
+  const [message, setMessage] = useState('')
   const [messages, setMessages] = useState([
     {
       name: "dog1",
       img: "https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      text: "Is this doggo?"
+      text: "Is this doggo?",
     },
     {
-      text: "Mebe UwU"
+      text: "Mebe UwU",
     },
   ]);
+  const handleSendMessage = (message) => {
+    socket.emit("message", message );
+    }
+  
   return (
     <div className="messageScreen">
       <h2>spicy DMs</h2>
-      <p className="messageScreen_match">You matched with {candidate.name} on {candidate.matchtime}</p>
-      {messages.map((message) => (
+      <p className="messageScreen_match">
+        You matched with {candidate.name} on {candidate.matchtime}
+      </p>
+      {messages.map((message) =>
         message.name ? (
-        <div className="messageScreen_msg">
-          <p className="messageScreen_text">{message.text}</p>
-        </div>
-        ) : (  <div className="messageScreen_msg">
-        <p className="messageScreen_textUser">{message.text}</p>
+          <div className="messageScreen_msg">
+            <p className="messageScreen_text">{message.text}</p>
+          </div>
+        ) : (
+          <div className="messageScreen_msg">
+            <p className="messageScreen_textUser">{message.text}</p>
+          </div>
+        )
+      )}
+      <div className="messageScreen_form">
+        <input
+          className="messageScreen_input"
+          type="text"
+          placeholder="Type a message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          className="messageScreen_button"
+          onClick={handleSendMessage}
+          disabled={message === "" ? true : false}
+        >
+          SEND
+        </button>
       </div>
-      )))}
     </div>
   );
 }
--- src/components/socketContext.js
+++ src/components/socketContext.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 const SocketContext = React.createContext()
 
 const SocketProvider = ({ children }) => {
-    const ENDPOINT = 'https://socket-chat-ak.herokuapp.com/';
+    const ENDPOINT = 'http://localhost:3001';
     const socket = io(ENDPOINT, { transports: ['websocket', 'polling'] })
     return (
         <SocketContext.Provider value={socket}>
