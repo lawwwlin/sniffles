07f378ecf27439074bad10b06c7f0cedb4b73116
Antony <ac.chan@live.com>
2021-09-02 08:58:44 +0000

    updated styling on most components

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,31 @@ import Button from "@material-ui/core/Button";
 import PetsIcon from "@material-ui/icons/Pets";
 import { Link } from "react-router-dom";
 
+import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  loginSubmit: {
+    background: theme.background,
+    border: 0,
+    fontSize: 16,
+    borderRadius: 6,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+}));
+
+function DeepChild() {
+  const classes = useStyles();
+
+  return (
+    <button type="button" className={classes.loginSubmit}>
+      LOG IN
+    </button>
+  );
+}
+
 function Home(props) {
   const [open, setOpen] = React.useState(false);
 
@@ -72,8 +97,23 @@ function Home(props) {
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={login} color="primary">
-              Log in
+            <Button onClick={login} >
+            <ThemeProvider
+        theme={{
+          background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        }}
+      >
+        <ThemeProvider
+          theme={(outerTheme) => ({
+            ...outerTheme,
+            background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+            boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
+          })}
+        >
+          <DeepChild />
+        </ThemeProvider>
+      </ThemeProvider>
             </Button>
           </DialogActions>
         </Dialog>
