f06d0e1ee7f10ec151447150f1eab32092fce8cd
Antony <ac.chan@live.com>
2021-09-04 01:53:33 +0000

    cleaned up chat window

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,26 +1,36 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { MainContext } from '../../mainContext';
-import { SocketContext } from '../../socketContext';
-import ScrollToBottom from 'react-scroll-to-bottom';
-import './Chat.scss';
-import { UsersContext } from '../../usersContext';
-import { TextField, Button } from '@material-ui/core';
-import SendIcon from '@material-ui/icons/Send';
-import MessageIcon from '@material-ui/icons/Message';
-import axios from 'axios';
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { MainContext } from "../../mainContext";
+import { SocketContext } from "../../socketContext";
+import ScrollToBottom from "react-scroll-to-bottom";
+import "./Chat.scss";
+import { UsersContext } from "../../usersContext";
+import { TextField, Button } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import MessageIcon from "@material-ui/icons/Message";
+import axios from "axios";
 import { useLocation, Link } from "react-router-dom";
 
 //info popup
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import Avatar from "@material-ui/core/Avatar";
+import PetsIcon from "@material-ui/icons/Pets";
 import IconButton from "@material-ui/core/IconButton";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import FlagIcon from '@material-ui/icons/Flag';
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import FlagIcon from "@material-ui/icons/Flag";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
 
 // required info: sender_id, receiver_id, room_id, sender_name
 const Chat = (props) => {
@@ -28,10 +38,11 @@ const Chat = (props) => {
   const { receiver_profile, sender_id, chatroom } = location.state;
   const { name, room, setName, setRoom } = useContext(MainContext);
   const socket = useContext(SocketContext);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState(JSON.parse(chatroom.messages));
   const { setUsers } = useContext(UsersContext);
   const history = useHistory();
+  const classes = useStyles();
 
   // open infobox
   const [open, setOpen] = React.useState(false);
@@ -56,9 +67,11 @@ const Chat = (props) => {
   const recipient = receiver_profile;
 
   // logout when the active history entry changes
-  window.onpopstate = e => logout();
+  window.onpopstate = (e) => logout();
   //Checks to see if there's a user present
-  useEffect(() => { if (!name) return history.push('/') }, [history, name]);
+  useEffect(() => {
+    if (!name) return history.push("/");
+  }, [history, name]);
 
   // useEffect(() => {
   //   socket.on("users", users => {
@@ -66,53 +79,75 @@ const Chat = (props) => {
   //   })
   // })
   useEffect(() => {
-    socket.on("message", msg => {
-      setMessages(messages => [...messages, msg])
+    socket.on("message", (msg) => {
+      setMessages((messages) => [...messages, msg]);
     });
-    console.log("stringify", JSON.stringify(messages))
-    console.log("msg from db", chatroom.messages)
+    console.log("stringify", JSON.stringify(messages));
+    console.log("msg from db", chatroom.messages);
     if (JSON.stringify(messages) !== chatroom.messages) {
       console.log("check msgs before axios:", messages);
-      const room = {id: chatroom.id, profile1_id: sender_id, profile2_id: recipient.id, messages: messages}
-      console.log('profile1', sender_id, 'profile2', recipient.id)
-      console.log('profile1', sender_id, 'profile2', recipient.id)
-      axios.put(`http://localhost:3001/api/chatroom/${chatroom.id}`, {room})
+      const room = {
+        id: chatroom.id,
+        profile1_id: sender_id,
+        profile2_id: recipient.id,
+        messages: messages,
+      };
+      console.log("profile1", sender_id, "profile2", recipient.id);
+      console.log("profile1", sender_id, "profile2", recipient.id);
+      axios
+        .put(`http://localhost:3001/api/chatroom/${chatroom.id}`, { room })
         .then((res) => {
-          console.log('chatroom saved?', res)
+          console.log("chatroom saved?", res);
         })
-        .catch(error => console.log(error));
+        .catch((error) => console.log(error));
     }
   }, [socket, messages]);
-  
-  
+
   const handleSendMessage = () => {
-    socket.emit('sendMessage', message, () => setMessage(''));
-    setMessage('');
+    socket.emit("sendMessage", message, () => setMessage(""));
+    setMessage("");
     console.log(message);
   };
 
   const logout = () => {
-    setName(''); 
-    setRoom('');
+    setName("");
+    setRoom("");
     // history.push('/messages');
     // history.go(0);
   };
 
   return (
-    <div className='room'>
-      <h4 className='heading'>
-        <div className='room-title'>
-          
-          <h3> room: {chatroom.id}, message to {recipient.name}</h3>
-          <div className='user-title'><h4>Current User: {name}</h4></div>
+    <div className="room">
+      <div className="heading">
+        <div className="room-title">
+          {console.log(`room: ${chatroom.id}, message to ${recipient.name}`)}
+
+          <IconButton onClick={handleClickOpen}>
+            <Avatar
+              id="chat_icon"
+              alt={recipient.name}
+              src={recipient.url}
+              style={{ fontSize: 40 }}
+              className={classes.large}
+            />
+          </IconButton>
+          <div>
+            {/* <p>{recipient.name}</p> */}
+          </div>
         </div>
-        <IconButton onClick={handleClickOpen}><InfoOutlinedIcon /></IconButton>
         {/* remove logout button later and add back button*/}
-        
-        <Link to='/messages'><Button onClick={logout}> 
-        Back
-        </Button></Link>
-      </h4>
+        <Link to="/candidate">
+          <IconButton>
+            <PetsIcon className="chat_logo" style={{ fontSize: 60 }} />
+          </IconButton>
+        </Link>
+
+        <Link to="/messages">
+          <IconButton onClick={logout}>
+            <ArrowBackIosIcon id="chat_icon" style={{ fontSize: 40 }} />
+          </IconButton>
+        </Link>
+      </div>
 
       <Dialog
         open={open}
@@ -123,58 +158,72 @@ const Chat = (props) => {
         <DialogTitle id="alert-dialog-title">{recipient.name}</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-          recipient info here
+            recipient info here
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-        <Button onClick={report} autoFocus>
-            <span className="chat_report"><FlagIcon/></span><p className="chat_report">REPORT</p>
+          <Button onClick={report} autoFocus>
+            <span className="chat_report">
+              <FlagIcon />
+            </span>
+            <p className="chat_report">REPORT</p>
           </Button>
           <Button onClick={remove} autoFocus>
-            <span className="chat_delete"><DeleteForeverIcon/></span><p className="chat_delete">REMOVE</p>
+            <span className="chat_delete">
+              <DeleteForeverIcon />
+            </span>
+            <p className="chat_delete">REMOVE</p>
           </Button>
         </DialogActions>
       </Dialog>
 
-      <ScrollToBottom className='messages' debug={false}>
-            <div className='match-time'>
-              <h4> You matched with {recipient.name} on -insert-timestamp-here- </h4>
-            </div>
-          {messages.length > 0 ?
-            messages.map((msg, i) =>
-            (<div key={i} className={`message ${msg.user === name ? "my-message" : ""}`}>
+      <ScrollToBottom className="messages" debug={false}>
+        <div className="match-time">
+          <h4>
+            {" "}
+            You matched with {recipient.name} on -insert-timestamp-here-{" "}
+          </h4>
+        </div>
+        {messages.length > 0 ? (
+          messages.map((msg, i) => (
+            <div
+              key={i}
+              className={`message ${msg.user === name ? "my-message" : ""}`}
+            >
               {console.log(msg)}
-              <h6 className='user'>{msg.user}</h6>
-              <h4 className='msg'>{msg.text}</h4>
-            </div>)
-            )
-            :
-            <div className='empty-message'>
-              <div>-----</div>
-              <MessageIcon />
-              <h4> No messages </h4>
-              <div>-----</div>
+              <h6 className="user">{msg.user}</h6>
+              <h4 className="msg">{msg.text}</h4>
             </div>
-          }
+          ))
+        ) : (
+          <div className="empty-message">
+            <div>-----</div>
+            <MessageIcon />
+            <h4> No messages </h4>
+            <div>-----</div>
+          </div>
+        )}
       </ScrollToBottom>
-      <div className='form'>
-        <TextField 
-          type="text" 
+      <div className="form">
+        <TextField
+          type="text"
           label="Enter Message"
-          value={message} onChange={e => setMessage(e.target.value)}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           variant="outlined"
         />
-        <Button 
+        <Button
           variant="contained"
-          color='primary' 
-          startIcon={<SendIcon />} 
-          onClick={handleSendMessage} 
-          disabled={message === '' ? true : false}>
+          color="primary"
+          startIcon={<SendIcon />}
+          onClick={handleSendMessage}
+          disabled={message === "" ? true : false}
+        >
           Send
-        </Button>  
+        </Button>
       </div>
     </div>
   );
 };
 
-export default Chat
+export default Chat;
