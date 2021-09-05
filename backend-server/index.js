773e8e397d21de8292792e4a42d031ed77c7c2b1
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-09-05 02:15:10 -0700

    add socket.io listener logout and delete user from socket once called

--- backend-server/index.js
+++ backend-server/index.js
@@ -39,13 +39,13 @@ io.on('connection', (socket) => {
     socket.in(room).emit('notification', { title: 'Someone\'s here', description: `${user.name} just entered the room` });
     io.in(room).emit('users', getUsers(room));
     callback();
-  })
+  });
 
   socket.on('sendMessage', message => {
       const user = getUser(socket.id);
       console.log(socket.id);
       io.in(user.room).emit('message', { user: user.name, text: message });
-  })
+  });
 
   socket.on("disconnect", () => {
       console.log("User disconnected");
@@ -54,7 +54,16 @@ io.on('connection', (socket) => {
         io.in(user.room).emit('notification', { title: 'Someone just left', description: `${user.name} just left the room` });
         io.in(user.room).emit('users', getUsers(user.room));
       }
-  })
+  });
+
+  socket.on("logout", () => {
+    console.log("User disconnected");
+    const user = deleteUser(socket.id);
+    if (user) {
+      io.in(user.room).emit('notification', { title: 'Someone just left', description: `${user.name} just left the room` });
+      io.in(user.room).emit('users', getUsers(user.room));
+    }
+});
 })
 
 app.get('/', (req, res) => {
