5150103de2774ff57b8f9017e5b6787d52ed2e82
cthu97 <85130439+cthu97@users.noreply.github.com>
2021-09-08 14:33:20 -0700

    update seeds again

--- backend-server/db/seeds/01_seeds.sql
+++ backend-server/db/seeds/01_seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO profile (name, breed, location, gender, age, size, owner, email, password, imageUrl, description)
 VALUES 
-('Bigboi', 'Maltese', 'Vancouver', 'male', 3, 'small', 'Cat', 'a@a.com', 'a', 'https://tinyurl.com/kb7dhhck', 'actually very smol'),
+('Bigboi', 'Maltese', 'Vancouver', 'male', 3, 'small', 'Cat', 'a@a.com', 'a', 'https://images.unsplash.com/photo-1577447310651-8453e529e838?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1080&q=80', 'actually very smol'),
 ('Nally', 'Husky', 'Vancouver Island Lighthouse Lab Basement', 'male', 5, 'large', 'Christian', 'b@b.com', '123', 'https://images.unsplash.com/photo-1489924034176-2e678c29d4c6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80', 'Monkey Fuzz!'),
 ('Lindsay', 'Labrador', 'Lighthouse Lab Basement', 'male', 1, 'small', 'Andy', 'c@c.com', '123', 'https://images.unsplash.com/photo-1512341350577-a09358311cb1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1260&q=80', 'I never go outside.'),
 ('Ace', 'Shiba Inu', 'Port Coquitlam', 'male', 5, 'medium', 'Luffy', 'd@d.com', '123', 'https://images.pexels.com/photos/4587993/pexels-photo-4587993.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'I like long walks'),
@@ -24,12 +24,12 @@ VALUES
 (true, 3, 1),
 (true, 1, 5),
 (true, 5, 1),
-(true, 6, 1),
-(true, 1, 6);
+(true, 7, 1),
+(true, 1, 7);
 
 INSERT INTO chatroom (profile1_id, profile2_id, messages)
 VALUES
 (1, 2, '[{"user":"Nally","text":"So what are your further developments?"}, {"user":"Bigboi","text":"Matching algorithm"}, {"user":"Bigboi","text":"Realtime notification on messages & matches"}, {"user":"Nally","text":"No way. Hold my beer."}]'),
 (1, 3, '[{"user":"Lindsay","text":"How was working as a team?"}, {"user":"Bigboi","text":"Me love team long time"}, {"user":"Lindsay","text":"Awesome!"}]'),
 (1, 5, '[{"user":"Jipp","text":"What tech stacks did you use?"}, {"user":"Bigboi","text":"React"}, {"user":"Bigboi","text":"(S)CSS"}, {"user":"Bigboi","text":"socket.io"}, {"user":"Bigboi","text":"Nodejs"},  {"user":"Bigboi","text":"Express"}, {"user":"Bigboi","text":"PSQL"}, {"user":"Jipp","text":"Kinda cool! I wonder what Kanye would say."}]'),
-(1, 6, '[{"user":"Sniffles","text":"Join sniffles room?"}, {"user":"Bigboi","text":"Join sniffles room"}, {"user":"Bigboi","text":"We have snacks"}, {"user":"Sniffles","text":"No we dont"}, {"user":"Bigboi","text":"Just bring your own then"},  {"user":"Sniffles","text":"Ok on my way"}, {"user":"Bigboi","text":"Hope to see everyone there!"}]');
+(1, 7, '[{"user":"Sniffles","text":"Join sniffles room?"}, {"user":"Bigboi","text":"Join sniffles room"}, {"user":"Bigboi","text":"We have snacks"}, {"user":"Sniffles","text":"No we dont"}, {"user":"Bigboi","text":"Just bring your own then"},  {"user":"Sniffles","text":"Ok on my way"}, {"user":"Bigboi","text":"Hope to see everyone there!"}]');
