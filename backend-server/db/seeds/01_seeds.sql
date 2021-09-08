bdbc97ae86f02aa71436e9c0bed960a0453fc7e6
cthu97 <85130439+cthu97@users.noreply.github.com>
2021-09-07 22:30:54 -0700

    seed change

--- backend-server/db/seeds/01_seeds.sql
+++ backend-server/db/seeds/01_seeds.sql
@@ -1,10 +1,10 @@
 INSERT INTO profile (name, breed, location, gender, age, size, owner, email, password, imageUrl, description)
 VALUES 
-('Bigboi', 'Maltese', 'Vancouver', 'male', 3, 'small', 'BigBoiOwner', 'a@a.com', 'a', 'https://tinyurl.com/kb7dhhck', 'actually very smol'),
-('Monty', 'Husky', 'Burnaby', 'male', 5, 'large', 'Dani', 'b@b.com', '123', 'https://images.unsplash.com/photo-1489924034176-2e678c29d4c6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80', 'I am doggo'),
-('Tyler', 'Labrador', 'Coquitlam', 'male', 1, 'small', 'Creator', 'c@c.com', '123', 'https://images.unsplash.com/photo-1512341350577-a09358311cb1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1260&q=80', 'looking for homies'),
+('Bigboi', 'Maltese', 'Vancouver', 'male', 3, 'small', 'Cat', 'a@a.com', 'a', 'https://tinyurl.com/kb7dhhck', 'actually very smol'),
+('Nally', 'Husky', 'Burnaby', 'male', 5, 'large', 'Dani', 'b@b.com', '123', 'https://images.unsplash.com/photo-1489924034176-2e678c29d4c6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80', 'I am doggo'),
+('Lindsay', 'Labrador', 'Coquitlam', 'male', 1, 'small', 'Creator', 'c@c.com', '123', 'https://images.unsplash.com/photo-1512341350577-a09358311cb1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1260&q=80', 'looking for homies'),
 ('Ace', 'Shiba Inu', 'Port Coquitlam', 'male', 5, 'medium', 'Luffy', 'd@d.com', '123', 'https://images.pexels.com/photos/4587993/pexels-photo-4587993.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'I like long walks'),
-('Diesel', 'Husky', 'Vancouver', 'male', 2, 'medium', 'Fam', 'e@e.com', '123', 'https://images.pexels.com/photos/4681107/pexels-photo-4681107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'We outside please'),
+('Jipp', 'Husky', 'Vancouver', 'male', 2, 'medium', 'Fam', 'e@e.com', '123', 'https://images.pexels.com/photos/4681107/pexels-photo-4681107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'We outside please'),
 ('Gucci', 'Maltese', 'Richmond', 'female', 1, 'small', 'Mane', 'f@f.com', '123', 'https://images.pexels.com/photos/2877086/pexels-photo-2877086.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Gimme food plz'),
 ('Casper', 'German Sheperd', 'New Westminster', 'male', 3, 'medium', 'Umi', 'g@g.com', '123', 'https://images.pexels.com/photos/6606825/pexels-photo-6606825.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'I want ball'),
 ('Lucky', 'Corgi', 'Richmond', 'male', 8, 'small', 'Oscar', 'h@h.com', '123', 'https://images.pexels.com/photos/164186/pexels-photo-164186.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Gimme treatz'),
@@ -27,5 +27,6 @@ VALUES
 
 INSERT INTO chatroom (profile1_id, profile2_id, messages)
 VALUES
-(1, 2, '[{"user":"Bigboi","text":"hi im Bigboi"}, {"user":"Monty","text":"hello there!"}, {"user":"Bigboi","text":"wanna meet somewhere?"}, {"user":"Monty","text":"sure!"}]'),
-(1, 3, '[{"user":"Bigboi","text":"hello"}, {"user":"Bigboi","text":"you are fat"}, {"user":"Biscuit","text":"no! im poofy"}]');
+(1, 2, '[{"user":"Nally","text":"So what are your further developments?"}, {"user":"Bigboi","text":"Matching algorithm"}, {"user":"Bigboi","text":"Realtime notification on messages & matches"}, {"user":"Nally","text":"No way. Hold my beer."}]'),
+(1, 3, '[{"user":"Lindsay","text":"How was working as a team?"}, {"user":"Bigboi","text":"Me love team long time"}]'),
+(1, 5, '[{"user":"Jipp","text":"What tech stacks did you use?"}, {"user":"Bigboi","text":"React"}, {"user":"Bigboi","text":"(S)CSS"}, {"user":"Bigboi","text":"socket.io"}, {"user":"Bigboi","text":"Nodejs"},  {"user":"Bigboi","text":"Express"}, {"user":"Bigboi","text":"PSQL"}, {"user":"Jipp","text":"Sweet"}, ]' );
